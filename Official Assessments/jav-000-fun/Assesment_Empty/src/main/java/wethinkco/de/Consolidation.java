5635196b11c248c7690b14dd6b701d75e1d5e4ee
jermaine <jermainebeea@gmail.com>
2025-07-13 21:39:56 +0200

    changes made

---
+++ Official Assessments/jav-000-fun/Assesment_Empty/src/main/java/wethinkco/de/Consolidation.java
@@ -0,0 +1,39 @@
+package wethinkco.de;
+
+public class Consolidation {
+
+    private static String fizzOnce(int number){
+        //Implementation of fizzOnce method
+        return "";
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        //Implementation of fizzBuzz method
+        return null;
+    }
+
+    public static String fibonacciSequence(int num){
+        //Implementation of fibonacciSequence method
+        return "";
+    }
+
+    public static String calculate(int a, int b, String operator){
+        //Implementation of calculate method
+        return "";
+    }
+
+    public static String drawTriangle(int num){
+        //Implementation of drawTriangle method
+        return "";
+    }
+
+    public static boolean isPalindrome(String str){
+        //Implementation of isPalindrome method
+        return false;
+    }
+
+    public static long factorial(int num){
+        //Implementation of factorial method
+        return 0L;
+    }
+}
