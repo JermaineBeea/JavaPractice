301722fb8bde76c36cc4aebe355c11a4b9f59a74
Jermaine Beea <159125926+JermaineBeea@users.noreply.github.com>
2026-02-19 12:23:38 +0000

    changes made

--- src/main/java/za/co/wethinkcode/Practice.java
+++ src/main/java/za/co/wethinkcode/Practice.java
@@ -1,17 +1,13 @@
 package za.co.wethinkcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 public class Practice{
 
     public static void main(String[] args) {
-        Object[] intArray = {11,2,3,4,5,'s'};
-       List<Object> digits = Arrays.asList(intArray);
-
-        String result = String.format("%d%d%d-%d%d%c", digits.toArray());
-
-        System.out.println(result);
+      
     }
 }
